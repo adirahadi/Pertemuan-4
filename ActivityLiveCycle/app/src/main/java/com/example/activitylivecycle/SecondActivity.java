060626a4b8009fd357b8598f2676546725051cba
adirahadi <116248549+adirahadi@users.noreply.github.com>
2023-01-09 23:04:48 +0700

    pertemuanempat

---
+++ ActivityLiveCycle/app/src/main/java/com/example/activitylivecycle/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.activitylivecycle;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+
+        }
+    }
+}
